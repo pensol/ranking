72a46e6287bf7ab109976994d96430547ead9099
Masahito”git config --global user.email hmasa91247844zkai@gmail.com” <hmasa91247844zkai@gmail.com>
2023-08-23 20:03:40 +0900

    Initial commit

--- server.js
+++ server.js
@@ -1,38 +1,47 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path'); 
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
-mongoose.connect('mongodb://localhost/ranking_db', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// MongoDB Atlas接続URI
+const uri = 'mongodb+srv://masahito:hirakegoma@ranking.jboipmw.mongodb.net/?retryWrites=true&w=majority';
+//mongodb+srv://masahito:<password>@ranking.jboipmw.mongodb.net/?retryWrites=true&w=majority
 
-const Rank = mongoose.model('Rank', {
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB Atlasに接続しました'))
+  .catch(err => console.error('MongoDB Atlasへの接続エラー:', err));
+
+// モデルの定義
+const DataSchema = new mongoose.Schema({
   username: String,
-  time: Number,
+  time: String
 });
 
-app.post('/submit', (req, res) => {
+const Data = mongoose.model('Data', DataSchema , 'ranking');
+
+// POSTリクエストを処理するエンドポイント
+app.post('/saveData', async (req, res) => {
   const { username, time } = req.body;
-  
-  if (!username || !time) {
-    return res.status(400).json({ error: 'Username and time are required' });
+  const data = new Data({ username, time });
+
+  try {
+    await data.save();
+    console.log('データが保存されました');
+    res.send('データが保存されました');
+  } catch (err) {
+    console.error('データ保存エラー:', err);
+    res.status(500).send('エラーが発生しました');
   }
-  
-  const rank = new Rank({ username, time });
-  rank.save()
-    .then(() => {
-      res.json({ message: 'Data saved successfully' });
-    })
-    .catch(err => {
-      res.status(500).json({ error: err.message });
-    });
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`サーバーがポート ${PORT} で起動しました`);
 });
